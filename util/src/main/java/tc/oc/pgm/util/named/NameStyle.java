450dd39fcd9fd6f6d72be32aa6020d65dd7d9fb3
applenick <applenick@users.noreply.github.com>
2021-02-05 14:34:17 -0800

    Hide nicked players from map contributions
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/named/NameStyle.java
+++ util/src/main/java/tc/oc/pgm/util/named/NameStyle.java
@@ -21,7 +21,7 @@ public enum NameStyle {
   // Fancy plus allowing disguised status reveal
   VERBOSE(EnumSet.of(Flag.COLOR, Flag.FLAIR, Flag.TELEPORT, Flag.DISGUISE, Flag.REVEAL)),
   // Fancy without teleport
-  CONCISE(EnumSet.of(Flag.COLOR, Flag.FLAIR, Flag.TELEPORT));
+  CONCISE(EnumSet.of(Flag.COLOR, Flag.FLAIR, Flag.TELEPORT, Flag.DISGUISE_OFFLINE));
 
   private final Set<Flag> flags;
 
