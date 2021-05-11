e6d1abe2f735a10dfa41307509696bf38832821c
applenick <applenick@users.noreply.github.com>
2021-05-11 00:50:20 -0700

    Support per-player component rendering
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/command/VotingCommand.java
+++ core/src/main/java/tc/oc/pgm/command/VotingCommand.java
@@ -4,6 +4,7 @@ import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
 import static net.kyori.adventure.text.event.ClickEvent.runCommand;
 import static net.kyori.adventure.text.event.HoverEvent.showText;
+import static tc.oc.pgm.api.text.PlayerComponent.player;
 
 import app.ashcon.intake.Command;
 import app.ashcon.intake.CommandException;
@@ -27,8 +28,8 @@ import tc.oc.pgm.rotation.pools.VotingPool;
 import tc.oc.pgm.rotation.vote.MapVotePicker;
 import tc.oc.pgm.rotation.vote.VotePoolOptions;
 import tc.oc.pgm.util.Audience;
-import tc.oc.pgm.util.UsernameFormatUtils;
 import tc.oc.pgm.util.named.MapNameStyle;
+import tc.oc.pgm.util.named.NameStyle;
 import tc.oc.pgm.util.text.TextFormatter;
 import tc.oc.pgm.util.text.TextTranslations;
 
@@ -52,7 +53,7 @@ public class VotingCommand {
         translatable(
             "vote.add",
             NamedTextColor.GRAY,
-            UsernameFormatUtils.formatStaffName(sender, match, sender),
+            player(sender, NameStyle.FANCY),
             map.getStyledName(MapNameStyle.COLOR));
 
     if (vote.isAdded(map)) {
@@ -85,7 +86,7 @@ public class VotingCommand {
           translatable(
               "vote.remove",
               NamedTextColor.GRAY,
-              UsernameFormatUtils.formatStaffName(sender, match, sender),
+              player(sender, NameStyle.FANCY),
               map.getStyledName(MapNameStyle.COLOR)),
           match);
     } else {
@@ -106,10 +107,7 @@ public class VotingCommand {
             NamedTextColor.LIGHT_PURPLE);
     ChatDispatcher.broadcastAdminChatMessage(
         translatable(
-            "vote.toggle",
-            NamedTextColor.GRAY,
-            UsernameFormatUtils.formatStaffName(sender, match, sender),
-            voteModeName),
+            "vote.toggle", NamedTextColor.GRAY, player(sender, NameStyle.FANCY), voteModeName),
         match);
   }
 
@@ -129,7 +127,7 @@ public class VotingCommand {
         translatable(
             "vote.remove",
             NamedTextColor.GRAY,
-            UsernameFormatUtils.formatStaffName(sender, match, sender),
+            player(sender, NameStyle.FANCY),
             TextFormatter.list(maps, NamedTextColor.GRAY));
 
     vote.clear();
