d9ace3d8b32962f0bf16411157c465aafc8538a3
applenick <applenick@users.noreply.github.com>
2021-05-16 00:23:27 -0700

    Add Punishment integration * Support for hidden players and cleaned up unused mute logic
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
+++ core/src/main/java/tc/oc/pgm/match/MatchPlayerImpl.java
@@ -206,6 +206,7 @@ public class MatchPlayerImpl implements MatchPlayer, Comparable<MatchPlayer> {
         spectatorTarget != null && spectatorTarget.getId().equals(other.getId());
     if (!other.isVisible() && !isSpectatorTarget) return false;
     if (other.isParticipating()) return true;
+    if (Integration.isHidden(other.getBukkit())) return false;
     if (Integration.isVanished(other.getBukkit()) && !getBukkit().hasPermission(Permissions.VANISH))
       return false;
     return isObserving()
