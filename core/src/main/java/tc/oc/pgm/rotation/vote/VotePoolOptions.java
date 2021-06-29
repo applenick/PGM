fc6dd162b4512047fb1ce0792673c5f7f29ed71b
applenick <applenick@users.noreply.github.com>
2021-06-28 18:08:11 -0700

    Implement request integration
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/rotation/vote/VotePoolOptions.java
+++ core/src/main/java/tc/oc/pgm/rotation/vote/VotePoolOptions.java
@@ -1,16 +1,20 @@
 package tc.oc.pgm.rotation.vote;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import tc.oc.pgm.api.map.MapInfo;
+import tc.oc.pgm.rotation.CustomVoteEntry;
 import tc.oc.pgm.rotation.pools.VotingPool;
 
 public class VotePoolOptions {
 
-  // Set of maps to be used in custom vote selection
-  private final Set<MapInfo> customVoteMaps;
+  private Set<CustomVoteEntry> customVoteMaps;
+
   // Whether custom map selection should replace existing entries
   private boolean replace;
 
@@ -32,24 +36,43 @@ public class VotePoolOptions {
     return replace;
   }
 
-  public boolean addVote(MapInfo map) {
-    if (customVoteMaps.size() < MapVotePicker.MAX_VOTE_OPTIONS) {
-      this.customVoteMaps.add(map);
-      return true;
+  public boolean addVote(MapInfo map, UUID playerId, boolean identify) {
+      if (customVoteMaps.size() < MapVotePicker.MAX_VOTE_OPTIONS) {
+        this.customVoteMaps.add(new CustomVoteEntry(map, identify, playerId));
+        return true;
+      }
+      return false;
+    }
+
+  public boolean removeMap(MapInfo map) {
+    Optional<CustomVoteEntry> entry =
+        getCustomVotes().stream().filter(s -> s.getMap().equals(map)).findFirst();
+    if (entry.isPresent()) {
+      return customVoteMaps.remove(entry.get());
     }
     return false;
   }
 
-  public boolean removeMap(MapInfo map) {
-    return this.customVoteMaps.remove(map);
-  }
-
-  public Set<MapInfo> getCustomVoteMaps() {
+  public Set<CustomVoteEntry> getCustomVotes() {
     return customVoteMaps;
   }
 
+  public Map<MapInfo, UUID> getOverrideMaps() {
+    Map<MapInfo, UUID> overrides = Maps.newHashMap();
+    customVoteMaps.stream()
+        .forEach(
+            entry -> {
+              overrides.put(entry.getMap(), entry.isIdentified() ? entry.getPlayerId() : null);
+            });
+    return overrides;
+  }
+
+  public Set<MapInfo> getCustomVoteMaps() {
+    return customVoteMaps.stream().map(CustomVoteEntry::getMap).collect(Collectors.toSet());
+  }
+
   public boolean isAdded(MapInfo info) {
-    return customVoteMaps.stream().anyMatch(s -> s.getName().equalsIgnoreCase(info.getName()));
+    return getCustomVoteMaps().stream().anyMatch(s -> s.getName().equalsIgnoreCase(info.getName()));
   }
 
   public void clear() {
@@ -57,7 +80,8 @@ public class VotePoolOptions {
   }
 
   public Map<MapInfo, Double> getCustomVoteMapWeighted() {
-    return customVoteMaps.stream()
-        .collect(Collectors.toMap(map -> map, x -> VotingPool.DEFAULT_SCORE));
+      return customVoteMaps.stream()
+          .collect(Collectors.toMap(map -> map.getMap(), x -> VotingPool.DEFAULT_SCORE));
   }
+  
 }
