f5f6ff707ccff25577531a7ce03bf795ac2cd90b
applenick <applenick@users.noreply.github.com>
2021-06-28 18:08:11 -0700

    Implement request integration
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/command/VotingCommand.java
+++ core/src/main/java/tc/oc/pgm/command/VotingCommand.java
@@ -22,6 +22,7 @@ import tc.oc.pgm.api.Permissions;
 import tc.oc.pgm.api.map.MapInfo;
 import tc.oc.pgm.api.map.MapOrder;
 import tc.oc.pgm.api.match.Match;
+import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.listeners.ChatDispatcher;
 import tc.oc.pgm.rotation.MapPoolManager;
 import tc.oc.pgm.rotation.pools.VotingPool;
@@ -42,18 +43,18 @@ public class VotingCommand {
       perms = Permissions.SETNEXT)
   public void addMap(
       Audience viewer,
-      CommandSender sender,
+      MatchPlayer sender,
       @Fallback(Type.NULL) @Text MapInfo map,
       MapOrder mapOrder,
       Match match)
       throws CommandException {
-    VotePoolOptions vote = getVoteOptions(sender, mapOrder);
+    VotePoolOptions vote = getVoteOptions(sender.getBukkit(), mapOrder);
 
     Component addMessage =
         translatable(
             "vote.add",
             NamedTextColor.GRAY,
-            player(sender, NameStyle.FANCY),
+            player(sender.getBukkit(), NameStyle.FANCY),
             map.getStyledName(MapNameStyle.COLOR));
 
     if (vote.isAdded(map)) {
@@ -61,7 +62,7 @@ public class VotingCommand {
       return;
     }
 
-    if (vote.addVote(map)) {
+    if (vote.addVote(map, sender.getId(), false)) {
       ChatDispatcher.broadcastAdminChatMessage(addMessage, match);
     } else {
       viewer.sendWarning(translatable("vote.limit", NamedTextColor.RED));
--- core/src/main/java/tc/oc/pgm/rotation/vote/MapPoll.java
+++ core/src/main/java/tc/oc/pgm/rotation/vote/MapPoll.java
@@ -8,6 +8,7 @@ import static net.kyori.adventure.text.event.ClickEvent.runCommand;
 import static net.kyori.adventure.text.event.HoverEvent.showText;
 import static net.kyori.adventure.title.Title.title;
 import static tc.oc.pgm.util.TimeUtils.fromTicks;
+import static tc.oc.pgm.util.text.PlayerComponent.player;
 import static tc.oc.pgm.util.text.TextTranslations.translate;
 
 import app.ashcon.intake.CommandException;
@@ -22,6 +23,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import net.kyori.adventure.inventory.Book;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
@@ -32,6 +34,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import com.google.common.collect.Maps;
+
 import tc.oc.pgm.api.Permissions;
 import tc.oc.pgm.api.map.MapInfo;
 import tc.oc.pgm.api.map.MapTag;
@@ -42,6 +47,7 @@ import tc.oc.pgm.api.setting.SettingKey;
 import tc.oc.pgm.api.setting.SettingValue;
 import tc.oc.pgm.util.inventory.tag.ItemTag;
 import tc.oc.pgm.util.named.MapNameStyle;
+import tc.oc.pgm.util.named.NameStyle;
 import tc.oc.pgm.util.text.TextTranslations;
 
 /** Represents a polling process, with a set of options. */
@@ -62,17 +68,23 @@ public class MapPoll {
   static final ItemTag<String> VOTE_BOOK_TAG = ItemTag.newString(VOTE_BOOK_METADATA);
 
   private final WeakReference<Match> match;
-
+  
   private final Map<MapInfo, Set<UUID>> votes;
+  private final Map<MapInfo, UUID> overrides;
   private boolean running = true;
 
   public MapPoll(Match match, List<MapInfo> maps) {
+      this(match, maps, Maps.newHashMap());
+  }
+  
+  public MapPoll(Match match, List<MapInfo> maps, Map<MapInfo, UUID> overrides) {
     this.match = new WeakReference<>(match);
     this.votes = new HashMap<>();
+    this.overrides = overrides;
     maps.forEach(m -> votes.put(m, new HashSet<>()));
 
     match.addListener(new VotingBookListener(this, match), MatchScope.LOADED);
-    match.getPlayers().forEach(viewer -> sendBook(viewer, false));
+    match.getPlayers().forEach(viewer -> sendBook(viewer, false));    
   }
 
   public void announceWinner(MatchPlayer viewer, MapInfo winner) {
@@ -109,11 +121,23 @@ public class MapPoll {
         .build();
   }
 
+  private Component getOverridePlayer(MapInfo map) {
+    UUID playerId = overrides.get(map);
+    Component playerName = null;
+
+    if (playerId != null && Bukkit.getPlayer(playerId) != null) {
+      playerName = player(playerId, NameStyle.FANCY);
+    }
+
+    return playerName;
+  }
+
   public void sendBook(MatchPlayer viewer, boolean forceOpen) {
     if (viewer.isLegacy()) {
       // Must use separate sendMessages, since 1.7 clients do not like the newline character
       viewer.sendMessage(VOTE_HEADER);
-      for (MapInfo pgmMap : votes.keySet()) viewer.sendMessage(getMapBookComponent(viewer, pgmMap));
+      for (MapInfo pgmMap : votes.keySet())
+        viewer.sendMessage(getMapBookComponent(viewer, pgmMap, getOverridePlayer(pgmMap)));
       return;
     }
 
@@ -122,7 +146,9 @@ public class MapPoll {
     content.append(newline());
 
     for (MapInfo pgmMap : votes.keySet())
-      content.append(newline()).append(getMapBookComponent(viewer, pgmMap));
+      content
+          .append(newline())
+          .append(getMapBookComponent(viewer, pgmMap, getOverridePlayer(pgmMap)));
 
     Book book = Book.builder().author(VOTE_BOOK_AUTHOR).pages(content.build()).build();
 
@@ -152,21 +178,35 @@ public class MapPoll {
     return personalDummyVoteBook;
   }
 
-  private Component getMapBookComponent(MatchPlayer viewer, MapInfo map) {
+  private Component getMapBookComponent(
+      MatchPlayer viewer, MapInfo map, @Nullable Component identified) {
     boolean voted = votes.get(map).contains(viewer.getId());
 
     TextComponent.Builder text = text();
+    TextComponent.Builder hover = text();
+
+    hover.append(
+        text(
+            map.getTags().stream().map(MapTag::toString).collect(Collectors.joining(" ")),
+            NamedTextColor.YELLOW));
+    if (identified != null) {
+      hover
+          .append(
+              text()
+                  .append(newline())
+                  .append(text("+", NamedTextColor.GREEN, TextDecoration.BOLD))
+                  .append(text("Sponsored by ", NamedTextColor.GRAY)))
+          .append(identified);
+    } // TODO: ^ hard coded text value, in the future make translatable or passed in so custom
+    // messages can be set
+
     text.append(
         text(
             voted ? SYMBOL_VOTED : SYMBOL_IGNORE,
             voted ? NamedTextColor.DARK_GREEN : NamedTextColor.DARK_RED));
     text.append(text(" ").decoration(TextDecoration.BOLD, !voted));
     text.append(text(map.getName(), NamedTextColor.GOLD, TextDecoration.BOLD));
-    text.hoverEvent(
-        showText(
-            text(
-                map.getTags().stream().map(MapTag::toString).collect(Collectors.joining(" ")),
-                NamedTextColor.YELLOW)));
+    text.hoverEvent(showText(hover.build()));
     text.clickEvent(runCommand("/votenext -o " + map.getName())); // Fix 1px symbol diff
     return text.build();
   }
