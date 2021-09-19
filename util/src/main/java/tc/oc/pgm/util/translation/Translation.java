34ea37ca4ace8f62febc92b674cb66722c23cb38
applenick <applenick@users.noreply.github.com>
2021-09-18 23:47:01 -0700

    Clean up translations and fix adventure-platform
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- util/src/main/java/tc/oc/pgm/util/translation/Translation.java
+++ util/src/main/java/tc/oc/pgm/util/translation/Translation.java
@@ -1,27 +1,27 @@
 package tc.oc.pgm.util.translation;
 
 import com.google.common.collect.Maps;
+import java.util.Locale;
 import java.util.Map;
 import org.bukkit.entity.Player;
+import tc.oc.pgm.util.text.TextTranslations;
 
 public class Translation {
 
-  private Player sender;
   private String message;
   private Map<String, String> translated;
 
-  public Translation(Player sender, String message) {
-    this.sender = sender;
+  public Translation(String message) {
     this.message = message;
     this.translated = Maps.newHashMap();
   }
 
-  public Player getSender() {
-    return sender;
-  }
-
   public String getMessage() {
     return message;
+  }
+
+  public String getMessage(Player player) {
+    return getMessage(getPlayerLanguageCode(player));
   }
 
   public String getMessage(String language) {
@@ -34,5 +34,14 @@ public class Translation {
 
   public void addTranslated(String language, String translatedMessage) {
     this.translated.put(language, translatedMessage);
+  }
+
+  public boolean isTranslated(String language) {
+    return translated.containsKey(language);
+  }
+
+  public static String getPlayerLanguageCode(Player player) {
+    Locale locale = TextTranslations.getNearestLocale(player.getLocale());
+    return locale.getLanguage();
   }
 }
