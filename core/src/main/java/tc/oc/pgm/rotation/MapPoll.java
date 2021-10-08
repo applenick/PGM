8276edc89e24c04f82238ee499f46726e4099d1b
applenick <applenick@users.noreply.github.com>
2021-10-07 22:56:16 -0700

    New event for map poll winners
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/rotation/MapPoll.java
+++ core/src/main/java/tc/oc/pgm/rotation/MapPoll.java
@@ -45,6 +45,7 @@ import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.api.setting.SettingKey;
 import tc.oc.pgm.api.setting.SettingValue;
+import tc.oc.pgm.events.MapVoteWinnerEvent;
 import tc.oc.pgm.util.inventory.tag.ItemTag;
 import tc.oc.pgm.util.named.MapNameStyle;
 import tc.oc.pgm.util.named.NameStyle;
@@ -321,6 +322,10 @@ public class MapPoll {
     Match match = this.match.get();
     if (match != null) {
       match.getPlayers().forEach(player -> announceWinner(player, picked));
+    }
+
+    if (picked != null) {
+      match.callEvent(new MapVoteWinnerEvent(picked));
     }
 
     updateScores();
