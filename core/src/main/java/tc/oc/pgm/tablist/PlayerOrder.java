c3390638073659b3a05b2cfec49adad0cdc9241e
applenick <applenick@users.noreply.github.com>
2021-10-23 01:53:40 -0700

    Resolve several tablist ordering issues * Thanks to Pablete1234!
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- core/src/main/java/tc/oc/pgm/tablist/PlayerOrder.java
+++ core/src/main/java/tc/oc/pgm/tablist/PlayerOrder.java
@@ -27,70 +27,46 @@ public class PlayerOrder implements Comparator<MatchPlayer> {
 
   @Override
   public int compare(MatchPlayer ma, MatchPlayer mb) {
-    if (this.viewer == ma) {
-      return -1;
-    } else if (this.viewer == mb) {
-      return 1;
-    }
+    if (this.viewer == ma) return -1;
+    else if (this.viewer == mb) return 1;
 
     Player a = ma.getBukkit();
     Player b = mb.getBukkit();
     Player viewer = this.viewer.getBukkit();
 
-    boolean aNicked = Integration.getNick(a) != null;
-    boolean bNicked = Integration.getNick(b) != null;
+    boolean isStaff = viewer.hasPermission(Permissions.STAFF);
 
-    boolean aStaff = a.hasPermission(Permissions.STAFF) && !aNicked;
-    boolean bStaff = b.hasPermission(Permissions.STAFF) && !bNicked;
-
+    // Friends are always first (assuming they see nicks)
     boolean aFriend = Integration.isFriend(viewer, a);
     boolean bFriend = Integration.isFriend(viewer, b);
+    if (aFriend && !bFriend) return -1;
+    else if (bFriend && !aFriend) return 1;
 
-    boolean aHidden = Integration.isHidden(a);
-    boolean bHidden = Integration.isHidden(b);
+    String aNick = aFriend || isStaff ? null : Integration.getNick(a);
+    String bNick = bFriend || isStaff ? null : Integration.getNick(b);
+    boolean aStaff = a.hasPermission(Permissions.STAFF) && aNick != null;
+    boolean bStaff = b.hasPermission(Permissions.STAFF) && bNick != null;
 
-    // Friends are always first :)
-    if (aFriend && !bFriend) {
-      return -1;
-    } else if (bFriend && !aFriend) {
-      return 1;
-    }
+    // Staff take priority, as long as nick is visible
+    if (aStaff && !bStaff) return -1;
+    else if (bStaff && !aStaff) return 1;
 
-    if (aNicked && !bNicked) {
-      return Integration.getNick(a).compareToIgnoreCase(b.getName(viewer));
-    } else if (bNicked && !aNicked) {
-      return a.getName(viewer).compareToIgnoreCase(Integration.getNick(b));
-    }
-
-    // Staff take priority, unless they are nicked
-    if (aStaff && !bStaff) {
-      return -1;
-    } else if (bStaff && !aStaff) {
-      return 1;
-    }
-
-    // Hidden players sorted last
-    if (aHidden && !bHidden) {
-      return 1;
-    } else if (bHidden && !aHidden) {
-      return -1;
-    }
+    // Short-circuit skipping groupcheck if both are nicked
+    if (aNick != null && bNick != null) return aNick.compareToIgnoreCase(bNick);
 
     // If players have different permissions, the player with the highest ranked perm
     // that the other one does't have is first. Disguised players effectively have no perms.
     for (Config.Group group : PGM.get().getConfiguration().getGroups()) {
       Permission permission = group.getPermission();
-      boolean aPerm = a.hasPermission(permission);
-      boolean bPerm = b.hasPermission(permission);
+      boolean aPerm = aNick != null && a.hasPermission(permission);
+      boolean bPerm = bNick != null && b.hasPermission(permission);
 
-      if (aPerm && !bPerm) {
-        return -1;
-      } else if (bPerm && !aPerm) {
-        return 1;
-      }
+      if (aPerm && !bPerm) return -1;
+      else if (bPerm && !aPerm) return 1;
     }
-
+    if (aNick == null) aNick = a.getName();
+    if (bNick == null) bNick = b.getName();
     // All else equal, order the players alphabetically
-    return a.getName().compareToIgnoreCase(b.getName());
+    return aNick.compareToIgnoreCase(bNick);
   }
 }
